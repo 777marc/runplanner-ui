68327b3f3d2d2245202622a408a7ae1800ad2bdf
777marc@gmail.com <777marc@gmail.com>
2021-08-11 20:39:58 -0400

    misc

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Menu from './components/app/Menu';
 
 function App() {
   return (
     <div className="App">
+      <Menu />
       Run Planner UI
     </div>
   );
---
+++ src/components/app/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Menu() {
+    return (
+        <div className="flex-container">
+            <p>home</p>
+            <p>workouts</p>
+            <p>training</p>
+        </div>
+    )
+}

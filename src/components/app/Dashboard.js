88966a4564ff50bf47e4e1c98852bd39bb77771a
777marc@gmail.com <777marc@gmail.com>
2021-08-21 12:13:02 -0400

    clean up

--- src/components/app/Dashboard.js
+++ src/components/app/Dashboard.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { get } from '../../data/DataService';
 
 export default function Dashboard() {
+
+    const [ workouts, setWorkouts ] = useState([]);
+
+    useEffect(() => {
+        get('workouts').then( res => {
+            setWorkouts(res.data.data);
+        })
+    });
+
     return (
         <div>
             dashboard
+
+            { 
+                workouts.map(wo => {
+                    return <p key={wo.id}>wo.name</p>
+                })
+            }
+
         </div>
     )
 }

9ba0c7dc77313b7967c17d6729ae9f805d68ceab
777marc@gmail.com <777marc@gmail.com>
2021-09-05 17:02:49 -0400

    workout row

--- src/components/workout/WorkoutRow.js
+++ src/components/workout/WorkoutRow.js
@@ -4,7 +4,9 @@ export default function WorkoutRow(props) {
     console.log(props)
     return (
         <div className="workout">
-            <p>{props.workout.name}</p>
+            {props.workout.name}
+            pace:{props.workout.pace}
+            duration:{props.workout.duration}
         </div>
     )
 }
